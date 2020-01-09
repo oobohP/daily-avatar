0b62e8704ce9c8e36e235e85f4ea1424244f75b1
sly96 <sLy@contractor.mcwe.com>
2020-01-09 00:52:33 -0800

    added comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json')
 
+// Creates the Discord Client Object
 const client = new Discord.Client();
 
+// Turning the bot "On"
 client.on('ready', () => {
     console.log('Bot Up and Running');
 });
