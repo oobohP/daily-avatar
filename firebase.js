e246caa9fa7a225188b4993354ad985ee6ea6557
sly96 <sLy@contractor.mcwe.com>
2020-01-12 01:12:42 -0800

    Bot or self user defined token in config.json is now able to pull images from firebase / GCS

--- firebase.js
+++ firebase.js
@@ -2,6 +2,11 @@
 var admin = require("firebase-admin");
 var serviceAccount = require("");
 
+// Exports
+module.exports = {
+  getURL
+};
+
 // Init firebase app with credentials and point to database url
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -21,12 +26,10 @@ function getAllFiles() {
 // Gets media link from firebase storage
 // TODO: Smarter way to check if file doesn't exist. Find a better way instead of math.rand through api.
 function getURL() {
-  bucket.file('IMG_' + Math.floor(Math.random() * Math.floor(25)) + '.png').getMetadata().then(response =>{
-    console.log(response[0]["mediaLink"])
-    return response[0]["mediaLink"];
-  }).catch(
-    console.log('no file like that exist, please check the database and see if it exist')
-  );
+  return new Promise(function(resolve, reject) {
+    bucket.file('IMG_' + Math.floor(Math.random() * Math.floor(8)) + '.JPEG').getMetadata().then(response =>{
+      // console.log(response[0]["mediaLink"])
+      resolve(response[0]["mediaLink"]);
+    });
+  })
 }
-
-getURL();
